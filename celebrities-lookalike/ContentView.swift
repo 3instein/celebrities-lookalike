09c5b54f087a30dc4e5a06340070f4f71a5f9d07
Reynaldi Kindarto <reynaldi_kindarto@yahoo.com>
2024-04-23 11:33:01 +0700

    Initial Commit

---
+++ celebrities-lookalike/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  celebrities-lookalike
+//
+//  Created by Reynaldi Kindarto on 23/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ celebrities-lookalike/celebrities_lookalikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  celebrities_lookalikeApp.swift
+//  celebrities-lookalike
+//
+//  Created by Reynaldi Kindarto on 23/04/24.
+//
+
+import SwiftUI
+
+@main
+struct celebrities_lookalikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
