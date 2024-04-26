b8bced929bf6d76bedfe7f90004d54a078a5ccf9
Reynaldi Kindarto <reynaldi_kindarto@yahoo.com>
2024-04-26 11:35:24 +0700

    Create Main Screen and Implement Open Camera (incl. Access)

--- celebrities-lookalike/ContentView.swift
+++ celebrities-lookalike/ContentView.swift
@@ -6,14 +6,45 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct ContentView: View {
+    @State private var showCamera = false
+    @State private var image: UIImage?
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image(uiImage: image ?? UIImage(named: "Logo")!)
+                .resizable()
+                .aspectRatio(contentMode: image != nil ? .fill : .fit) // Adjust content mode based on whether image is set
+                .frame(width: 300, height: 300)
+                .clipShape(Rectangle())
+                .overlay(Rectangle().stroke(Color.black, lineWidth: 2))
+
+            HStack {
+                Spacer().frame(width: 75)
+                
+                Button(action: {
+                    ImagePicker.checkPermissionsAndOpenCamera(showCamera: showCamera) { success in
+                        if success {
+                            self.showCamera = true
+                        }
+                    }
+                }) {
+                    Image(systemName: "camera")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 75, height: 75)
+                }.sheet(isPresented: $showCamera) {
+                    ImagePicker(image: self.$image, showCamera: self.$showCamera)
+                }
+                
+                Image(systemName: "photo")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 50, height: 50)
+            }
+
         }
         .padding()
     }
