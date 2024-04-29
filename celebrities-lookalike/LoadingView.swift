246ad0d259285e45b7b57e62e2af3d470a8c8b5e
Reynaldi Kindarto <reynaldi_kindarto@yahoo.com>
2024-04-29 11:27:05 +0700

    Add LoadingView.swift for displaying a loading animation

---
+++ celebrities-lookalike/LoadingView.swift
@@ -0,0 +1,33 @@
+//
+//  LoadingView.swift
+//  celebrities-lookalike
+//
+//  Created by Reynaldi Kindarto on 29/04/24.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    @State private var rotation: Double = 0
+    
+    var body: some View {
+        VStack {
+            ZStack {
+                Image("Logo")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit) // Adjust content mode based on whether image is set
+                    .frame(width: 300, height: 300)
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.black, lineWidth: 2))
+                    .rotationEffect(.degrees(rotation))
+                    .animation(Animation.linear(duration: 5).repeatForever(autoreverses: false))
+                    .onAppear {
+                        self.rotation = 360
+                    }
+            }
+            Text("Analyzing...")
+                .font(.title)
+                .padding()
+        }
+    }
+}
