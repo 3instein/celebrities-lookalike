c9618bc6168335e312f3a0c536a56896f760e67a
Reynaldi Kindarto <reynaldi_kindarto@yahoo.com>
2024-04-29 11:27:09 +0700

    Refactor ContentView.swift to improve image loading and identification process

--- celebrities-lookalike/ContentView.swift
+++ celebrities-lookalike/ContentView.swift
@@ -9,50 +9,74 @@ import SwiftUI
 import AVFoundation
 
 struct ContentView: View {
-    @State private var showCamera = false
+    @State private var showCamera: Bool = false
     @State private var image: UIImage?
+    @State private var isLoading: Bool = false
+    @State private var identificationResult: [[String: String]]?
     
     var body: some View {
-        VStack {
-            Image(uiImage: image ?? UIImage(named: "Logo")!)
-                .resizable()
-                .aspectRatio(contentMode: image != nil ? .fill : .fit) // Adjust content mode based on whether image is set
-                .frame(width: 300, height: 300)
-                .clipShape(Rectangle())
-                .overlay(Rectangle().stroke(Color.black, lineWidth: 2))
-
-            HStack {
-                Spacer().frame(width: image != nil ? 75 : 0)
-                
-                Button(action: {
-                    ImagePicker.checkPermissionsAndOpenCamera(showCamera: showCamera) { success in
-                        if success {
-                            self.showCamera = true
-                        }
-                    }
-                }) {
-                    Image(systemName: "camera")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 75, height: 75)
-                }.sheet(isPresented: $showCamera) {
-                    ImagePicker(image: self.$image, showCamera: self.$showCamera)
-                }
-                
-                if(image != nil) {
-                    Button(action: {
-                        identify(image: (image ?? UIImage(systemName: "logo"))!)
-                    }) {
-                        Image(systemName: "arrow.right")
+        
+        if(isLoading){
+            LoadingView()
+        } else {
+            if (identificationResult == nil){
+                NavigationView {
+                    VStack {
+                        Image(uiImage: image ?? UIImage(named: "Logo")!)
                             .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 75, height: 75)
-                    }
-                }
+                            .aspectRatio(contentMode: image != nil ? .fill : .fit) // Adjust content mode based on whether image is set
+                            .frame(width: 300, height: 300)
+                            .clipShape(Rectangle())
+                            .overlay(Rectangle().stroke(Color.black, lineWidth: 2))
+                        
+                        HStack {
+                            Spacer().frame(width: image != nil ? 75 : 0)
+                            
+                            Button(action: {
+                                ImagePicker.checkPermissionsAndOpenCamera(showCamera: showCamera) { success in
+                                    if success {
+                                        self.showCamera = true
+                                    }
+                                }
+                            }) {
+                                Image(systemName: "camera")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 75, height: 75)
+                            }.sheet(isPresented: $showCamera) {
+                                ImagePicker(image: self.$image, showCamera: self.$showCamera)
+                            }
+                            
+                            if let image = image {
+                                Button(action: {
+                                    self.isLoading = true
+                                    identify(image: image) { result, error in
+                                        if let error = error {
+                                            print("Error: \(error.localizedDescription)")
+                                            // Handle error
+                                        } else if let result = result {
+                                            // Handle result
+                                            self.identificationResult = result
+                                            print(result)
+                                            // Set isLoading to false here if needed
+                                        }
+                                        self.isLoading = false
+                                    }
+                                }) {
+                                    Image(systemName: "arrow.right")
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .frame(width: 75, height: 75)
+                                }
+                            }
+                        }
+                        
+                    }.padding()
+                }.navigationBarTitle("Identify Celebrities")
+            } else {
+                ResultView(baseImage: $image, result: $identificationResult)
             }
-
         }
-        .padding()
     }
 }
 
